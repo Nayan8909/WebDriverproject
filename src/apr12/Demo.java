3638d11162dec3407a94c26d5483647c098bf116
Bhairavnath <Bhairavnath@DESKTOP-ASMHM00>
2024-09-26 20:26:21 +0530

    New class with methods added

---
+++ src/apr12/Demo.java
@@ -0,0 +1,17 @@
+package apr12;
+
+public class Demo {
+public void add()
+{
+	int a=345, b=56,c ; 
+	c=a+b;
+	System.out.println(c);
+}
+
+public void mul()
+{
+	int a=345, b=56,c ; 
+	c=a*b;
+	System.out.println(c);
+}
+}
